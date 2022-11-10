c469e904a265565760eee585a43978c7536e0482
karachevtsev <karachevtsev@yahoo.com>
2022-11-10 22:55:00 +0200

    Added views and templates

--- app_blog/models.py
+++ app_blog/models.py
@@ -9,12 +9,21 @@ from django.urls import reverse
 class Category(models.Model):
     category = models.CharField(u'Категорія', max_length=250, help_text=u'Максимум 250 символів')
     slug = models.SlugField(u'Слаг')
+    objects = models.Manager()
 
     class Meta:
         verbose_name = u'Категорія для публікації'
         verbose_name_plural = u'Категорії для публікацій'
 
-    def str(self): return self.category
+    def __str__(self):
+        return self.category
+
+    def get_absolute_url(self):
+        try:
+            url = reverse('articles-category-list', kwargs={'slug': self.slug})
+        except:
+            url = "/"
+        return url
 
 
 class Article(models.Model):
